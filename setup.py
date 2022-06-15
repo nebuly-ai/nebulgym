8cce891de7e193a1527a202d95df200fd21673bc
morgoth95 <d.fiori@nebuly.ai>
2022-06-15 15:28:27 +0200

    change release version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="nebulgym",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     long_description=long_description,
